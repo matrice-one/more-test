ee59e8418d170aff9e3f89e5908329f9e7285ac3
neigeline-rivollat <88484229+neigeline-rivollat@users.noreply.github.com>
2021-12-20 10:13:54 +0100

    no

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 colors = {
-    'background': '#1d492b',
+    'background': '#1d492b',x
     'text': '#7FDBFF'
 }
 
