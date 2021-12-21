21490bb40c8bd27919dbda9e81e08a667d2ab6cd
neigeline-rivollat <88484229+neigeline-rivollat@users.noreply.github.com>
2021-12-21 12:38:28 +0100

    hh

--- app.py
+++ app.py
@@ -4,13 +4,27 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World')
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value'),
+        html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
